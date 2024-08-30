ba7c0f4260e7148f7cd5e86b5d87b8e09f45a9f2
Hwodza <hwodza@gmail.com>
2024-08-29 22:03:08 -0400

    created base.html and filled in basic login page

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username == 'admin' and password == 'admin':
-            return redirect(url_for('home'))
-        else:
-            flash('Invalid Credentials')
     return render_template('login.html')
 
 @app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
